f4a777fcf078639ca54b0ca6b50ffd42bbc9f15f
Joe Jheng <halflucifer@gmail.com>
2022-11-03 14:07:31 +0800

    Threesome goes well

--- src/find_all_three_sum.rs
+++ src/find_all_three_sum.rs
@@ -10,10 +10,6 @@ pub fn three_sum(arr: &[i32], target: i32) -> Vec<Vec<i32>> {
     input.sort();
 
     while index < input.len() {
-        while index + 1 < input.len() && input[index] == input[index + 1] {
-            index += 1;
-        }
-
         let curr = input[index];
         let sum = target - curr;
         let two_sum = find_all_two_sum(&input[index + 1..], sum);
@@ -21,6 +17,10 @@ pub fn three_sum(arr: &[i32], target: i32) -> Vec<Vec<i32>> {
         two_sum.iter().for_each(|p| {
             result.push(vec![curr, p.0, p.1]);
         });
+
+        while index + 1 < input.len() && input[index] == input[index + 1] {
+            index += 1;
+        }
 
         index += 1;
     }
@@ -50,11 +50,11 @@ fn find_all_two_sum(arr: &[i32], target: i32) -> Vec<(i32, i32)> {
 
             result.push((l, r));
 
-            while arr[left] == l {
+            while arr[left] == l && left < arr.len() - 1 {
                 left += 1;
             }
 
-            while arr[right] == r {
+            while arr[right] == r && right > 0 {
                 right -= 1;
             }
         }
@@ -82,14 +82,14 @@ mod tests {
         assert_eq!(three_sum(&[], 0), expected);
         assert_eq!(three_sum(&[0], 0), expected);
         assert_eq!(three_sum(&[0, 0], 0), expected);
-        assert_eq!(three_sum(&[0, 0, 0], 0), expected);
+        assert_eq!(three_sum(&[0, 0, 0], 0), [[0, 0, 0]]);
         assert_eq!(three_sum(&[0, i32::MAX, i32::MIN + 1], 0), [[i32::MIN + 1, 0, i32::MAX]]);
     }
 
     #[test]
     fn test_repeated_input_cases() {
-        assert_eq!(three_sum(&[0, 0, 0, 1, 1, 1, -1, -1, -1], 0), [[-1, 0, 1]]);
-        assert_eq!(three_sum(&[-2, 1, 0, 0, 0, 1, -1, 2], 0), [[-2, 0, 2], [-2, 1, 1], [-1, 0, 1]]);
+        assert_eq!(three_sum(&[0, 0, 0, 1, 1, 1, -1, -1, -1], 0), [[-1, 0, 1], [0, 0, 0]]);
+        assert_eq!(three_sum(&[-2, 1, 0, 0, 0, 1, -1, 2], 0), [[-2, 0, 2], [-2, 1, 1], [-1, 0, 1], [0, 0, 0]]);
 
         let mut input = vec![];
         for i in 0..6 {
@@ -98,6 +98,6 @@ mod tests {
                 input.push(-i);
             }
         }
-        assert_eq!(three_sum(&input, 0), [[-5, 1, 4], [-5, 2, 3], [-4, -1, 5], [-4, 1, 3], [-4, 2, 2], [-3, -2, 5], [-3, -1, 4], [-3, 1, 2], [-2, -1, 3]]);
+        assert_eq!(three_sum(&input, 0), [[-5, 1, 4], [-5, 2, 3], [-4, -1, 5], [-4, 1, 3], [-4, 2, 2], [-3, -2, 5], [-3, -1, 4], [-3, 1, 2], [-2, -2, 4], [-2, -1, 3]]);
     }
 }
