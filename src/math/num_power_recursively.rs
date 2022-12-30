a9170ae6f9a6b288eba76d7b5448d70a82bd512a
Joe Jheng <halflucifer@gmail.com>
2022-12-30 11:52:08 +0800

    Enhance the robustness

--- src/math/num_power_recursively.rs
+++ src/math/num_power_recursively.rs
@@ -1,13 +1,36 @@
-pub fn num_power_recursively(base: u64, power: u32) -> u64 {
-    if power == 0 {
-        return 1;
+pub fn num_power_recursively(base: f64, power: i32) -> f64 {
+    if base == 0.0 {
+        return 0.0;
+    } else if base == 1.0 {
+        return 1.0;
+    } else if base == -1.0 {
+        if power % 2 == 0 {
+            return 1.0;
+        } else {
+            return -1.0;
+        }
     }
 
-    if power % 2 == 0 {
-        let half = num_power_recursively(base, power / 2);
-        half * half
-    } else {
-        base * num_power_recursively(base, power - 1)
+    match power {
+        i32::MIN | i32::MAX => 0.0,
+        0 => 1.0,
+        1 => base,
+        _ => {
+            let m = power.abs();
+            let mut res = num_power_recursively(base, m / 2);
+
+            res *= res;
+
+            if m % 2 == 1 {
+                res *= base;
+            }
+
+            if power < 0 {
+                res = 1.0 / res;
+            }
+
+            res
+        }
     }
 }
 
@@ -17,30 +40,27 @@ mod tests {
 
     #[test]
     fn test_normal_cases() {
-        assert_eq!(num_power_recursively(1, 100), 1);
-        assert_eq!(num_power_recursively(2, 50), 2_u64.pow(50));
-        assert_eq!(num_power_recursively(3, 40), 3_u64.pow(40));
+        assert_eq!(num_power_recursively(1.0, 100), 1.0);
+        assert_eq!(num_power_recursively(2.0, 50), 2_f64.powf(50.0));
+        assert_eq!(num_power_recursively(3.0, 40), 3_f64.powf(40.0));
         
-        assert_eq!(num_power_recursively(4, 30), 4_u64.pow(30));
-        assert_eq!(num_power_recursively(5, 20), 5_u64.pow(20));
-        assert_eq!(num_power_recursively(6, 10), 6_u64.pow(10));
-
-        for i in 0..=10 {
-            assert_eq!(num_power_recursively(2, i), 2_u64.pow(i));
-            assert_eq!(num_power_recursively(10, i), 10_u64.pow(i));
-            assert_eq!(num_power_recursively(i as u64, i), (i as u64).pow(i));
-        }
+        assert_eq!(num_power_recursively(4.0, 30), 4_f64.powf(30.0));
+        assert_eq!(num_power_recursively(5.0, 20), 5_f64.powf(20.0));
+        assert_eq!(num_power_recursively(6.0, 10), 6_f64.powf(10.0));
     }
 
     #[test]
     fn test_edge_cases() {
-        assert_eq!(num_power_recursively(0, 0), 0_u64.pow(0));
-        assert_eq!(num_power_recursively(0, 1), 0_u64.pow(1));
+        assert_eq!(num_power_recursively(0.0, 0), 0.0);
+        assert_eq!(num_power_recursively(0.0, 1), 0.0);
 
-        assert_eq!(num_power_recursively(0, u32::MAX), 0_u64.pow(u32::MAX));
-        assert_eq!(num_power_recursively(1, u32::MAX), 1_u64.pow(u32::MAX));
+        assert_eq!(num_power_recursively(0.0, i32::MAX), 0.0);
+        assert_eq!(num_power_recursively(0.0, i32::MIN), 0.0);
 
-        assert_eq!(num_power_recursively(u64::MAX, 0), u64::MAX.pow(0));
-        assert_eq!(num_power_recursively(u64::MAX, 1), u64::MAX.pow(1));
+        assert_eq!(num_power_recursively(1.0, i32::MAX), 1.0);
+        assert_eq!(num_power_recursively(1.0, i32::MIN), 1.0);
+        
+        assert_eq!(num_power_recursively(-1.0, i32::MAX), -1.0);
+        assert_eq!(num_power_recursively(-1.0, i32::MIN), 1.0);
     }
 }
