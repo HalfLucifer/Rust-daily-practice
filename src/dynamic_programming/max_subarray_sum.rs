9aa17820953e7f8e45951cb37a2bab612ba315ef
Joe Jheng <halflucifer@gmail.com>
2023-01-16 08:59:20 +0800

    Be better, and stronger

--- src/dynamic_programming/max_subarray_sum.rs
+++ src/dynamic_programming/max_subarray_sum.rs
@@ -1,19 +1,46 @@
-pub fn max_subarray_sum(arr: &[i32]) -> i32 {
-    if arr.is_empty() {
-        return 0;
+/*
+    Given an integer array nums, find the subarray with the largest sum and return its sum
+*/
+
+/*
+    Bottom-up DP method
+    - Time complexity: O(n)
+    - Space complexity: O(n)
+*/
+pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+    // DP table: dp[i] is the largest subarray sum ended at i
+    let mut dp = vec![0; nums.len()];
+    let mut res = nums[0];
+
+    // Base case: nums[0] is the sum ended at 0
+    dp[0] = nums[0];
+
+    for i in 1..nums.len() {
+        // DP trasition
+        dp[i] = nums[i].max(dp[i - 1] + nums[i]);
+
+        // Update the solution
+        res = res.max(dp[i]);
     }
 
-    let mut prev = arr[0];
-    let mut curr = arr[0];
-    let mut result = arr[0];
+    res
+}
 
-    for i in 1..arr.len() {
-        curr = arr[i].max(arr[i] + prev);
-        result = result.max(curr);
-        prev = curr;
+/*
+    Space optimized DP method
+    - Time complexity: O(n)
+    - Space complexity: O(1)
+*/
+pub fn max_sub_array_optimized(nums: Vec<i32>) -> i32 {
+    let mut prev_num = nums[0];
+    let mut max_sum = nums[0];
+
+    for i in 1..nums.len() {
+        prev_num = nums[i] + if prev_num > 0 { prev_num } else { 0 };
+        max_sum = max_sum.max(prev_num);
     }
 
-    result
+    max_sum
 }
 
 #[cfg(test)]
@@ -22,19 +49,31 @@ mod tests {
 
     #[test]
     fn test_normal_cases() {
-        assert_eq!(max_subarray_sum(&[-3, 1, 3, -1, 2, -4, 2]), 5);
-        assert_eq!(max_subarray_sum(&[0, 1, -1, 0, 1, -1, 0, 1, -1]), 1);
-        assert_eq!(max_subarray_sum(&[-1, -2, -3, -4, -5]), -1);
-        assert_eq!(max_subarray_sum(&[-4, -3, -2, -1, 0]), 0);
-        assert_eq!(max_subarray_sum(&[-23, 30, -36, 42, -55, 62, -71, 88]), 88);
+        assert_eq!(max_sub_array(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+        assert_eq!(max_sub_array(vec![5, 4, -1, 7, 8]), 23);
+        assert_eq!(max_sub_array(vec![-3, 1, 3, -1, 2, -4, 2]), 5);
+        assert_eq!(max_sub_array(vec![0, 1, -1, 0, 1, -1, 0, 1, -1]), 1);
+        assert_eq!(max_sub_array(vec![-1, -2, -3, -4, -5]), -1);
+        assert_eq!(max_sub_array(vec![-4, -3, -2, -1, 0]), 0);
+        assert_eq!(max_sub_array(vec![-23, 30, -36, 42, -55, 62, -71, 88]), 88);
+    }
+
+    #[test]
+    fn test_space_op_cases() {
+        assert_eq!(max_sub_array_optimized(vec![-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6);
+        assert_eq!(max_sub_array_optimized(vec![5, 4, -1, 7, 8]), 23);
+        assert_eq!(max_sub_array_optimized(vec![-3, 1, 3, -1, 2, -4, 2]), 5);
+        assert_eq!(max_sub_array_optimized(vec![0, 1, -1, 0, 1, -1, 0, 1, -1]), 1);
+        assert_eq!(max_sub_array_optimized(vec![-1, -2, -3, -4, -5]), -1);
+        assert_eq!(max_sub_array_optimized(vec![-4, -3, -2, -1, 0]), 0);
+        assert_eq!(max_sub_array_optimized(vec![-23, 30, -36, 42, -55, 62, -71, 88]), 88);
     }
 
     #[test]
     fn test_edge_cases() {
-        assert_eq!(max_subarray_sum(&[]), 0);
-        assert_eq!(max_subarray_sum(&[0]), 0);
-        assert_eq!(max_subarray_sum(&[-1, 0]), 0);
-        assert_eq!(max_subarray_sum(&[0, 1]), 1);
-        assert_eq!(max_subarray_sum(&[i32::MIN, 0, i32::MAX]), i32::MAX);
+        assert_eq!(max_sub_array(vec![1]), 1);
+        assert_eq!(max_sub_array(vec![-1, 0]), 0);
+        assert_eq!(max_sub_array(vec![0, 1]), 1);
+        assert_eq!(max_sub_array(vec![i32::MIN, 0, i32::MAX]), i32::MAX);
     }
 }
